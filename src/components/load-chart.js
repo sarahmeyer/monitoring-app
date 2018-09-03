8302658bd1d6d1bad0c6d25c5ecc3d820d3c9603
Sarah Meyer <sarahsaltrick@gmail.com>
2018-09-03 16:33:50 -0400

    monitoring app: update ui every 10 s

--- src/components/load-chart.js
+++ src/components/load-chart.js
@@ -20,13 +20,30 @@ class LoadChart extends React.Component {
 		});
 	}
 
+	componentDidUpdate() {
+		setTimeout(() => {
+			const db = new PouchDB('http://localhost:5984/pouchdb__records');
+			db.allDocs({
+				include_docs: true,
+			}).then((result) => {
+				if (this.state.records.length !== result.rows.length) {
+					this.setState({
+						records: result.rows,
+					});
+				}
+			}).catch((err) => {
+				console.log('error fetching records', err);
+			});
+		}, 10000);
+	}
+
 	render() {
 		return (
 			<div>
 				{
 					this.state.records.map((record) => {
 						return (
-							<div key={`record_${record.doc.timestamp}`}>
+							<div key={`record_${record.id}`}>
 								<span>{record.doc.oneMinuteLoad}</span>
 								<span>{record.doc.fiveMinuteLoad}</span>
 								<span>{record.doc.fifteenMinuteLoad}</span>
