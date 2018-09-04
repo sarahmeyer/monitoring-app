86f87962500dcc56cc7b3b6237c60f5ccdeb600c
Sarah Meyer <sarahsaltrick@gmail.com>
2018-09-04 12:25:14 -0400

    monitoring app: install d3-scale, start drawing

--- src/components/load-chart.js
+++ src/components/load-chart.js
@@ -1,56 +1,75 @@
 import * as React from 'react';
 import PouchDB from 'pouchdb';
+import { scaleTime } from 'd3-scale';
 
+const DEFAULT_WIDTH = 300;
+const DEFAULT_HEIGHT = 200;
 
 class LoadChart extends React.Component {
 	state = {
 		records: [],
 	}
 
-	componentDidMount() {
+	setupCanvas() {
+	    const canvas = this.refs.canvas;
+	    const ctx = canvas.getContext("2d");
+
+	    const canvasWidth = canvas.attributes.width.value;
+	    const canvasHeight = canvas.attributes.height.value;
+
+		ctx.beginPath();
+		ctx.moveTo(0, canvasHeight);
+		ctx.lineTo(canvasWidth, canvasHeight);
+		ctx.stroke();
+	}
+
+	drawTicks(records) {
+
+	}
+
+	plotRecordsOnCanvas(records) {
+	    const canvas = this.refs.canvas;
+	    const ctx = canvas.getContext("2d");
+
+	    const canvasWidth = canvas.attributes.width.value;
+	    const canvasHeight = canvas.attributes.height.value;
+		debugger;
+
+	}
+
+	fetchRecords() {
+		const { records } = this.state;
+
 		const db = new PouchDB('http://localhost:5984/pouchdb__records');
 		db.allDocs({
 			include_docs: true,
 		}).then((result) => {
-			this.setState({
-				records: result.rows,
-			});
+			if (records.length !== result.rows.length) {
+				this.setState({
+					records: result.rows,
+				}, () => {
+					this.plotRecordsOnCanvas(result.rows);
+				});
+			}
 		}).catch((err) => {
 			console.log('error fetching records', err);
 		});
 	}
 
+	componentDidMount() {
+		this.fetchRecords();
+
+		this.setupCanvas();
+	}
+
 	componentDidUpdate() {
-		setTimeout(() => {
-			const db = new PouchDB('http://localhost:5984/pouchdb__records');
-			db.allDocs({
-				include_docs: true,
-			}).then((result) => {
-				if (this.state.records.length !== result.rows.length) {
-					this.setState({
-						records: result.rows,
-					});
-				}
-			}).catch((err) => {
-				console.log('error fetching records', err);
-			});
-		}, 10000);
+		setTimeout(this.fetchRecords, 10000);
 	}
 
 	render() {
 		return (
 			<div>
-				{
-					this.state.records.map((record) => {
-						return (
-							<div key={`record_${record.id}`}>
-								<span>{record.doc.oneMinuteLoad}</span>
-								<span>{record.doc.fiveMinuteLoad}</span>
-								<span>{record.doc.fifteenMinuteLoad}</span>
-							</div>
-						);
-					})
-				}
+				<canvas ref="canvas" width={DEFAULT_WIDTH} height={DEFAULT_HEIGHT} />
 			</div>
 		);
 	}
